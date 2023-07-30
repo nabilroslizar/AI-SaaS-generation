d9dda6ec398cb17b20f1ca3fd60761db9082959a
nabilroslizar95 <nabil.roslizar@gmail.com>
2023-07-30 22:51:23 +0800

    conversation ui

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,21 +1,34 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
 
 const LandingPage = () => {
-  return (
-    <div>
-      Landing Page (Unprotected)
+  const router = useRouter();
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  if (!userId)
+    return (
       <div>
-        <Link href="/sign-in">
-          <Button>Log In</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
+        Landing Page (Unprotected)
+        <div>{userId}</div>
+        <div>
+          <Link href="/sign-in">
+            <Button>Log In</Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button>Register</Button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+
+  if (userId) {
+    router.push("/dashboard");
+  }
+  return <div>Hello {userId}</div>;
 };
 
 export default LandingPage;
